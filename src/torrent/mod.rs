91722d7bbb8376bbf07e7eb75a30336fad606381
Luminarys <postmaster@gensok.io>
2018-01-03 10:08:49 -0800

    Always announce torrent completion to tracker

--- src/torrent/mod.rs
+++ src/torrent/mod.rs
@@ -500,12 +500,10 @@ impl<T: cio::CIO> Torrent<T> {
     }
     /// Signal that we've downloaded and verified the torrent
     fn set_finished(&mut self) {
-        // Only send complete to the tracker if we've downloaded
-        // everything - may not happen from 0 priority pieces
-        if self.pieces.complete() {
-            let req = tracker::Request::completed(self);
-            self.cio.msg_trk(req);
-        }
+        // It's ok to say we've completed even if we haven't downloaded everything since
+        // the `left` field should indicate how much there still is to download.
+        let req = tracker::Request::completed(self);
+        self.cio.msg_trk(req);
         // Order here is important, if we're in an idle status,
         // rpc updates don't occur.
         self.update_rpc_transfer();
