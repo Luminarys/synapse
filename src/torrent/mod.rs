6afbf241c79457506f4b5923bdc1558726659d09
Luminarys <postmaster@gensok.io>
2018-01-10 20:32:29 -0800

    Revert "Ensure artifact deletion removes all directories"
    This reverts commit 3fa7a79ff896f2bdc31513476736960170c54348 due to
    panics on empty file lists and potentially removing user added files.

--- src/torrent/mod.rs
+++ src/torrent/mod.rs
@@ -4,7 +4,7 @@ pub mod bitfield;
 mod picker;
 mod choker;
 
-use std::{fmt, path};
+use std::fmt;
 use std::collections::BTreeMap;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
@@ -344,13 +344,14 @@ impl<T: cio::CIO> Torrent<T> {
 
     pub fn delete(&mut self, artifacts: bool) {
         debug!("Sending file deletion request!");
-        let comp = self.info.files[0].path.components().next().unwrap();
-        let dirp: &path::Path = comp.as_ref().as_ref();
-        let dir: path::PathBuf = dirp.to_owned();
+        let mut files = Vec::new();
+        for file in &self.info.files {
+            files.push(file.path.clone());
+        }
         self.cio.msg_disk(disk::Request::delete(
             self.id,
             self.info.hash,
-            dir,
+            files,
             self.path.clone(),
             artifacts,
         ));
