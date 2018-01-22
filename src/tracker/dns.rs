57c58dc19ed8974f4a9627bb88fb299b7089e128
Luminarys <postmaster@gensok.io>
2018-01-21 21:26:11 -0800

    Simplify DNS handling

--- src/tracker/dns.rs
+++ src/tracker/dns.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::net::IpAddr;
 use std::sync::{Arc, Mutex};
 use std::os::unix::io::{AsRawFd, RawFd};
@@ -17,10 +16,8 @@ pub struct QueryResponse {
 pub struct Resolver {
     reg: amy::Registrar,
     socks: FHashMap<usize, c_ares::Socket>,
-    csocks: FHashMap<c_ares::Socket, usize>,
     chan: c_ares::Channel,
     sender: Arc<Mutex<amy::Sender<QueryResponse>>>,
-    marked: HashSet<usize>,
 }
 
 struct CSockWrapper(c_ares::Socket);
@@ -38,10 +35,8 @@ impl Resolver {
         Resolver {
             reg,
             socks: FHashMap::default(),
-            csocks: FHashMap::default(),
             chan: c_ares::Channel::with_options(opts).unwrap(),
             sender: Arc::new(Mutex::new(send)),
-            marked: HashSet::new(),
         }
     }
 
@@ -58,37 +53,20 @@ impl Resolver {
     }
 
     pub fn tick(&mut self) {
-        self.marked.clear();
         // Add any unrecognized fd's to amy for polling, remove
         // any fd's in amy which are no longer handled by c_ares
-        for (fd, _, _) in &self.chan.get_sock() {
-            // Not efficient, but the max number of queries active at at time is likely limited
-            if let Some(id) = self.csocks.get(&fd).cloned() {
-                self.marked.insert(id);
-            } else {
-                let id = self.reg
-                    .register(&CSockWrapper(fd), amy::Event::Both)
-                    .unwrap();
-                self.marked.insert(id);
-                self.socks.insert(id, fd);
-                self.csocks.insert(fd, id);
-            }
-        }
-
-        // Any socks remaining in csocks should be deregistered and destroyed
-        let socks = &mut self.socks;
-        let csocks = &mut self.csocks;
-        let marked = &self.marked;
-        let reg = &self.reg;
-        socks.retain(|id, fd| {
-            if marked.contains(id) {
-                true
-            } else {
-                csocks.remove(fd);
-                reg.deregister(&CSockWrapper(*fd)).ok();
-                false
-            }
-        });
+        self.socks = self.chan
+            .get_sock()
+            .iter()
+            .map(|(fd, _, _)| {
+                (
+                    self.reg
+                        .register(&CSockWrapper(fd), amy::Event::Both)
+                        .unwrap(),
+                    fd,
+                )
+            })
+            .collect();
     }
 
     pub fn new_query(&mut self, id: usize, host: &str) {
