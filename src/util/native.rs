65157c981c31d5ff9bd4732d8568d25f5ea9c6a6
Luminarys <postmaster@gensok.io>
2018-01-25 20:07:37 -0800

    Fall back to truncation on fallocate failure

--- src/util/native.rs
+++ src/util/native.rs
@@ -9,11 +9,14 @@ extern "C" {
     fn native_fallocate(fd: c_int, len: u64) -> c_int;
 }
 
-// TODO: Consider doing the stupid fallback of zeroing the file out.
 pub fn fallocate(f: &File, len: u64) -> io::Result<()> {
     // We ignore the len here, if you actually have a u64 max, then you're kinda fucked either way.
     match unsafe { native_fallocate(f.as_raw_fd(), len) } {
         0 => Ok(()),
-        e => Err(io::Error::from_raw_os_error(e)),
+        e => {
+            error!("fallocate failed: {}", e);
+            f.set_len(len)?;
+            Ok(())
+        }
     }
 }
