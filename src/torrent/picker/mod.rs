58257e5da414864cfc9543388b88d7ede3eb4d7f
Luminarys <postmaster@gensok.io>
2018-02-28 01:37:44 -0800

    Fix picker tests

--- src/torrent/picker/mod.rs
+++ src/torrent/picker/mod.rs
@@ -413,7 +413,7 @@ impl Picker {
             pieces,
             &vec![3u8; info.files.len()],
         );
-        p.change_picker(true);
+        p.change_picker(true, pieces);
         p
     }
 }
