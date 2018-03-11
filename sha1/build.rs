5f994583ba95e40b53bb06f49549b03119f78f94
Luminarys <postmaster@gensok.io>
2018-03-11 04:12:53 -0700

    Disable asm sha1 rounds on OSX

--- sha1/build.rs
+++ sha1/build.rs
@@ -1,7 +1,10 @@
 extern crate cc;
 
 fn main() {
-    let asm_path = if cfg!(target_arch = "x86") {
+
+    let asm_path = if cfg!(target_os = "macos") {
+        "src/generic.c"
+    } else if cfg!(target_arch = "x86") {
         "src/x86.S"
     } else if cfg!(target_arch = "x86_64") {
         "src/x64.S"
