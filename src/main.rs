634ee2a62ad82459e434f5e5bd91294b4b56a3bb
Luminarys <postmaster@gensok.io>
2019-06-09 13:25:23 -0700

    Update Peer ID to 0010

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ lazy_static! {
         use rand::{self, Rng};
 
         let mut pid = [0u8; 20];
-        let prefix = b"-SY0002-";
+        let prefix = b"-SY0010-";
         for i in 0..prefix.len() {
             pid[i] = prefix[i];
         }
